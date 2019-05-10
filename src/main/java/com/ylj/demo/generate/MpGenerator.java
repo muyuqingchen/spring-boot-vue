0c0c5b76978f8f2eb0ddb063b96d6f29db163757
yinglinjiangbt <yinglinjiangbt@hikbt.com>
2019-05-10 08:33:02 +0800

    [mod]整合mybatis plus

---
+++ src/main/java/com/ylj/demo/generate/MpGenerator.java
@@ -0,0 +1,10 @@
+package com.ylj.demo.generate;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+
+public class MpGenerator {
+
+    public static void main(String[] args) {
+    }
+}
